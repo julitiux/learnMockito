a146fe4c11507de4371d8833660e91482ca8c682
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-11-29 17:18:42 -0600

    starting chapter 1 Build a Testing Foundation from Mockito

---
+++ src/main/java/com/learn_mockito/LearnMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.learn_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn_mockito/LearnMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
