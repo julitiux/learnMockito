e6293c3708b2a619b384ca3737c1a40a40889d10
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-06 15:07:45 -0600

    implement HelloMockito class

---
+++ src/main/java/com/learn_mockito/chapter1/HelloMockito.java
@@ -0,0 +1,22 @@
+package com.learn_mockito.chapter1;
+
+import java.util.Optional;
+
+public class HelloMockito {
+
+  private String greeting = "Hello, %s, from Mockiot!";
+
+  private final PersonRespository personRespository;
+  private final TranslationService translationService;
+
+  public HelloMockito(PersonRespository personRespository, TranslationService translationService) {
+    this.personRespository = personRespository;
+    this.translationService = translationService;
+  }
+
+  public String greet(Long id, String sourceLang, String targetLang) {
+    Optional<Person> person = personRespository.findById(id);
+    String name = person.map(Person::getFirstName).orElse("World");
+    return translationService.translate(String.format(greeting, name), sourceLang, targetLang);
+  }
+}
