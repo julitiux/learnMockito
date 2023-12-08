8762676bdca9d591802b23f6d77b55c9efe01164
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-07 23:18:15 -0600

    fixing problems into Person entity

--- src/main/java/com/learn_mockito/chapter1/Person.java
+++ src/main/java/com/learn_mockito/chapter1/Person.java
@@ -1,21 +1,27 @@
 package com.learn_mockito.chapter1;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "person")
 public class Person {
 
+  @Id
+  @GeneratedValue
   private Long id;
   private String firstName;
   private String lastName;
   private String mail;
 
+  public String getFirstName() {
+    return firstName;
+  }
 }
