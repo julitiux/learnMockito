9b13ae7c1f50fe80d49eccc283bfdd4da395351c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-13 01:08:14 -0600

    rename file src/test/java/com/learn_mockito/chapter1/HelloMockitoTest.java -> src/test/java/com/learn_mockito/chapter1/HelloWorldTest.java

--- src/test/java/com/learn_mockito/chapter1/HelloWorldTest.java
+++ src/test/java/com/learn_mockito/chapter1/HelloWorldTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HelloMockitoTest {
+public class HelloWorldTest {
 
   private HelloWorld helloWorld = new HelloWorld();
 
