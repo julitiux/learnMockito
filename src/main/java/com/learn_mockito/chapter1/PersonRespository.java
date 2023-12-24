329e01b887e9bdfb98299d4801b2c57112cd6c77
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-23 18:03:50 -0600

    declare methods like findById, findAll, save and delete. Delete all about JpaRepository

--- src/main/java/com/learn_mockito/chapter1/PersonRespository.java
+++ src/main/java/com/learn_mockito/chapter1/PersonRespository.java
@@ -1,6 +1,13 @@
 package com.learn_mockito.chapter1;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
-public interface PersonRespository extends JpaRepository<Person, Long> {
+public interface PersonRespository {
+
+  Optional<Person> findById(Long id);
+  List<Person> findAll();
+  Person saver(Person person);
+  void delete(Person person);
+
 }
