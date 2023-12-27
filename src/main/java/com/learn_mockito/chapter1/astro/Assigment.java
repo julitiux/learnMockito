4e8bcc7a4d57129de9a7c743b2a55712ba7d9295
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-26 20:06:54 -0600

    implement class Assigment, add tags like Data and AllArgsConstructor

---
+++ src/main/java/com/learn_mockito/chapter1/astro/Assigment.java
@@ -0,0 +1,13 @@
+package com.learn_mockito.chapter1.astro;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Assigment {
+
+  private final String name;
+  private final String craft;
+
+}
