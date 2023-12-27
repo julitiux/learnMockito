d9622b3d70356cb3b02cbc5f6309ed20bc1dc9c6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-26 20:16:08 -0600

    implement class AstroService

---
+++ src/main/java/com/learn_mockito/chapter1/astro/AstroService.java
@@ -0,0 +1,27 @@
+package com.learn_mockito.chapter1.astro;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class AstroService {
+
+  private final Gateway<AstroResponse> gateway;
+
+  public Map<String, Long> getAstroData() {
+    AstroResponse response = gateway.getResponse();
+    return groupByCraft(response);
+  }
+
+  private Map<String, Long> groupByCraft(AstroResponse data) {
+    return data.getPeople().stream()
+      .collect(
+        Collectors.groupingBy(
+          Assigment::getCraft, Collectors.counting()
+        )
+      );
+  }
+
+}
