a8f91728969c27949943e353232a40124389f12c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-28 19:24:48 -0600

    implement Person class for chapter2

---
+++ src/main/java/com/learn_mockito/chapter2/Person.java
@@ -0,0 +1,17 @@
+package com.learn_mockito.chapter2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class Person {
+
+  private final Integer id;
+  private final String first;
+  private final String last;
+  private final LocalDate localDate;
+
+}
