66b2cae773eb938d9672c503d3a068b3bc7868c2
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-03 23:28:43 -0600

    implement PersonRepository Test

--- src/test/java/com/learn_mockito/chapter2/PersonServiceTest.java
+++ src/test/java/com/learn_mockito/chapter2/PersonServiceTest.java
@@ -4,20 +4,23 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
 
   @Test
-  public void defaultImplementations(){
+  public void defaultImplementations() {
     PersonRepository mockRepo = mock(PersonRepository.class);
 
     assertAll(
-      () -> assertNull(mockRepo.save(any(Person.class)))
+      () -> assertNull(mockRepo.save(any(Person.class))),
+      () -> assertTrue(mockRepo.findById(anyInt()).isEmpty()),
+      () -> assertTrue(mockRepo.findAll().isEmpty()),
+      () -> assertEquals(0, mockRepo.count())
     );
 
   }
