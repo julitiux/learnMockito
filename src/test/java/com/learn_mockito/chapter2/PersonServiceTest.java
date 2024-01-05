bbece258ee934a25f37d442b4745505eb54b2a42
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-01-04 18:23:08 -0600

    implement test getLastNames_usingMockMethod()

--- src/test/java/com/learn_mockito/chapter2/PersonServiceTest.java
+++ src/test/java/com/learn_mockito/chapter2/PersonServiceTest.java
@@ -9,10 +9,11 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PersonServiceTest {
@@ -24,6 +25,27 @@ public class PersonServiceTest {
     new Person(14, "Anita", "Borg", LocalDate.of(1949, Month.JANUARY, 17)),
     new Person(5, "Liskov", "Borg", LocalDate.of(1939, Month.NOVEMBER, 7))
   );
+
+  @Test
+  public void getLastNames_usingMockMethod() {
+    // Create a stub for the PersonRepository
+    PersonRepository mockRepo = mock(PersonRepository.class);
+
+    // Set the expectations on the mock...
+    when(mockRepo.findAll()).thenReturn(people);
+
+    // Inject the mock into the service
+    PersonService personService = new PersonService(mockRepo);
+
+    // Get the last names (this is the method to test)
+    List<String> lastNames = personService.getLastNames();
+
+    // Check that the last names are correct ()using AssertJ)
+    assertThat(lastNames).contains("Borg", "Goldberg", "Hopper", "Liskov", "Lovelace");
+
+    // Verify that the service called findAll on the mockRepo exactly once
+    verify(mockRepo).findAll();
+  }
 
   @Test
   public void defaultImplementations() {
