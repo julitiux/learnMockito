879ffa538650530daab04bb9f35a87046d5b7ad5
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-30 00:36:07 -0600

    add comments

--- src/main/java/com/learn_mockito/HelloMockitoRevised.java
+++ src/main/java/com/learn_mockito/HelloMockitoRevised.java
@@ -6,14 +6,17 @@ public class HelloMockitoRevised {
 
   private String greeting = "Hello, %s, from Mockito!";
 
+  // Dependencies
   private final PersonRepository personRepository;
   private final TranslationService translationService;
 
+  // Constructor to inject the dependencies
   public HelloMockitoRevised(PersonRepository personRepository, TranslationService translationService) {
     this.personRepository = personRepository;
     this.translationService = translationService;
   }
 
+  // Method ww want to test
   public String greet(int id, String sourceLang, String targetLang) {
     Optional<Person> person = personRepository.findById(id);
     String name = person.map(Person::getFirst).orElse("World");
