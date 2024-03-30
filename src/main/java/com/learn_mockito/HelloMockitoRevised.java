35e54d44cdcdcc4be718532ffa02f6810e9e4549
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-30 00:34:05 -0600

    implement method greet with params like id, sourceLang and targetLang

--- src/main/java/com/learn_mockito/HelloMockitoRevised.java
+++ src/main/java/com/learn_mockito/HelloMockitoRevised.java
@@ -1,5 +1,7 @@
 package com.learn_mockito;
 
+import java.util.Optional;
+
 public class HelloMockitoRevised {
 
   private String greeting = "Hello, %s, from Mockito!";
@@ -7,9 +9,15 @@ public class HelloMockitoRevised {
   private final PersonRepository personRepository;
   private final TranslationService translationService;
 
-
   public HelloMockitoRevised(PersonRepository personRepository, TranslationService translationService) {
     this.personRepository = personRepository;
     this.translationService = translationService;
   }
+
+  public String greet(int id, String sourceLang, String targetLang) {
+    Optional<Person> person = personRepository.findById(id);
+    String name = person.map(Person::getFirst).orElse("World");
+    return translationService.translate(String.format(greeting, name), sourceLang, targetLang);
+  }
+
 }
