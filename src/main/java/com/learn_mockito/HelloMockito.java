7988be427bc30c61358787a474ff0889998783c3
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-29 23:45:26 -0600

    implement class HelloMockito.java. That class welcomes the user with a simple String returned from a greet method

---
+++ src/main/java/com/learn_mockito/HelloMockito.java
@@ -0,0 +1,10 @@
+package com.learn_mockito;
+
+public class HelloMockito {
+
+  private String greeting = "Hello %s, from Mockito!";
+
+  public String greet(String name) {
+    return String.format(greeting, name);
+  }
+}
