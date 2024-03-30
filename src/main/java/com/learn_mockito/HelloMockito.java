a393d74ab763ebf94ba8ca32bb34d46353c81e6c
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-03-30 00:05:04 -0600

    add just a coma

--- src/main/java/com/learn_mockito/HelloMockito.java
+++ src/main/java/com/learn_mockito/HelloMockito.java
@@ -2,7 +2,7 @@ package com.learn_mockito;
 
 public class HelloMockito {
 
-  private String greeting = "Hello %s, from Mockito!";
+  private String greeting = "Hello, %s, from Mockito!";
 
   public String greet(String name) {
     return String.format(greeting, name);
