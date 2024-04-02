23775388571c88c003a619b0038b6fb563d1d77f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-04-01 22:29:48 -0600

    implement class Assignment to astro

---
+++ src/main/java/com/learn_mockito/astro/Assignment.java
@@ -0,0 +1,13 @@
+package com.learn_mockito.astro;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Assignment {
+
+  private final String name;
+  private final String craft;
+
+}
